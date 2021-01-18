56757dc0fc53d5367c6abdf8cb0202635a099f1d
maetaro <maetaro@users.noreply.github.com>
2021-01-18 11:13:50 +0900

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -8,4 +8,14 @@ module.exports = {
     "^.+\\.(ts|tsx)$": "ts-jest",
   },
   collectCoverage: true,
+  coverageDirectory: "dist/coverage",
+  coverageReporters: ["text", "html"],
+  coverageThreshold: {
+    global: {
+      branches: 50,
+      functions: 50,
+      lines: 50,
+      statements: 50,
+    },
+  },
 };
