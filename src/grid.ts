8fb2153c5c9d65cdda4d7792b59857f180eb0a62
maetaro <maetaro@users.noreply.github.com>
2021-01-18 14:02:14 +0900

    Update grid.ts

--- src/grid.ts
+++ src/grid.ts
@@ -1,20 +1,51 @@
+/**
+ * Code blocks are great for examples
+ * This comment _supports_ [Markdown](https://marked.js.org/)
+ *
+ * ```
+ * <my-custom-element>Highlight JS will auto detect the language</my-custom-element>
+ * ```
+ *
+ * ```typescript
+ * // Or you can specify the language explicitly
+ * const instance = new MyClass();
+ * ```
+ */
 export class Grid {
+  /**
+   * Overall width
+   */
   width: number;
+  /**
+   * Number to divide
+   */
   columns: number;
-  constructor(width: number, columns?: number) {
-    if (!columns) {
-      columns = 16;
-    }
+  /**
+   * Grid class constructor
+   * @param width Overall width
+   * @param columns Number to divide
+   */
+  constructor(width: number, columns: number = 16) {
     this.width = width;
     this.columns = columns;
   }
+  /**
+   * Returns the specified position. width separated by columns.
+   * @param index 
+   */
   span(index: number): number {
     return (this.width / this.columns) * index;
   }
-  center(offset?: number): number {
-    if (!offset) {
-      offset = 0;
-    }
+  /**
+   * Returns the center position.
+   * @param offset distance from center.
+   * ```typescript
+   * > const instance = new Grid(30, 3);
+   * > grid.center()
+   * 15
+   * ```
+   */
+  center(offset: number = 0): number {
     return (this.width / this.columns) * (this.columns / 2.0 + offset);
   }
 }
