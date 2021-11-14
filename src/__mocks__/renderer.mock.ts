46ec6d2d72ac904b9271cda3be8501b47d4b51dc
maetaro <maetaro0614@gmail.com>
2021-11-14 20:02:10 +0900

    Delete renderer.mock.ts

--- src/__mocks__/renderer.mock.ts
+++
@@ -1,7 +0,0 @@
-// Import this named export into your test file:
-export const mockPlaySoundFile = jest.fn();
-const mock = jest.fn().mockImplementation(() => {
-  return { playSoundFile: mockPlaySoundFile };
-});
-
-export default mock;
